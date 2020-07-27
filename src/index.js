bfb083d0e800860202ac294663da270237862611
Lnola <nola.luka@gmail.com>
2020-07-27 21:03:36 +0200

    Initial architecture

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import App from "./App";
+import "./styles.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
