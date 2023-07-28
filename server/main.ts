597ac3a9640535a6c8dc0912ab5d902b0ac0a3b2
Lnola <nola.luka@gmail.com>
2023-07-28 22:52:39 +0200

    :sparkles: Get video with ytdl-core

--- server/main.ts
+++ server/main.ts
@@ -1,1 +1,7 @@
-console.log('Hello world');
+// import fs from 'fs';
+import ytdl from 'ytdl-core';
+
+const videoUrl = 'https://www.youtube.com/watch?v=xvFZjo5PgG0';
+
+const stream = ytdl(videoUrl, { filter: 'audioonly' });
+console.log(stream);
