19efe96a055b01ce3dd4a7a4af0a2cbf3dba8b85
Lnola <nola.luka@gmail.com>
2023-07-28 23:03:17 +0200

    :sparkles: Add ffmpeg to convert file to mp3

--- server/main.ts
+++ server/main.ts
@@ -1,7 +1,24 @@
-// import fs from 'fs';
+import fs from 'fs';
 import ytdl from 'ytdl-core';
+import ffmpeg from 'fluent-ffmpeg';
 
+const outputFile = 'audio.mp3';
 const videoUrl = 'https://www.youtube.com/watch?v=xvFZjo5PgG0';
 
 const stream = ytdl(videoUrl, { filter: 'audioonly' });
-console.log(stream);
+
+ffmpeg(stream)
+  .toFormat('mp3')
+  .on('error', (err) => {
+    console.error('An error occurred:', err.message);
+  })
+  .on('end', () => {
+    console.log('Finished converting to MP3 format.');
+  })
+  .pipe(fs.createWriteStream(outputFile))
+  .on('error', (err) => {
+    console.error('An error occurred while saving the file:', err.message);
+  })
+  .on('finish', () => {
+    console.log('File saved successfully.');
+  });
