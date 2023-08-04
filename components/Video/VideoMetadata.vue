f6544578d3ba6bd7d4f5ae9290eb97d767549e27
Lnola <nola.luka@gmail.com>
2023-08-04 23:14:18 +0200

    :sparkles: Add a video metadata form component

---
+++ components/Video/VideoMetadata.vue
@@ -0,0 +1,16 @@
+<template>
+  <form @submit.prevent="submit">
+    <YouInput v-model="metadata.artist" name="artist" label="Artist" />
+    <YouInput v-model="metadata.category" name="category" label="Category" />
+    <YouInput v-model="metadata.title" name="title" label="Title" />
+    <YouButton type="submit">Submit</YouButton>
+  </form>
+</template>
+
+<script setup>
+const metadata = useMetadata();
+
+const submit = async () => {
+  console.log(metadata.value);
+};
+</script>
