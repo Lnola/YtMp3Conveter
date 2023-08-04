bfd16b96681246fa51dd104798cd1ae0772058c8
Lnola <nola.luka@gmail.com>
2023-08-04 23:14:02 +0200

    :sparkles: Add a video url form component

---
+++ components/Video/VideoUrl.vue
@@ -0,0 +1,22 @@
+<template>
+  <form @submit.prevent="submit">
+    <YouInput v-model="videoUrl" name="video-link" label="Youtube video link" />
+    <YouButton type="submit">Submit</YouButton>
+  </form>
+</template>
+
+<script setup lang="ts">
+const emits = defineEmits(['fetching']);
+
+const videoUrl = ref('');
+const metadata = useMetadata();
+
+const submit = async () => {
+  emits('fetching', true);
+  const query = { videoUrl: videoUrl.value };
+  const data = await $fetch('/api/audio/metadata', { query });
+  metadata.value = data;
+  videoUrl.value = '';
+  emits('fetching', false);
+};
+</script>
