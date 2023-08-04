25732ebbe91c9663f487b88f7ab97cb4baf1682f
Lnola <nola.luka@gmail.com>
2023-08-04 22:21:42 +0200

    :sparkles: Create a basic button component

---
+++ components/YouButton.vue
@@ -0,0 +1,7 @@
+<template>
+  <button
+    class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none"
+  >
+    <slot></slot>
+  </button>
+</template>
