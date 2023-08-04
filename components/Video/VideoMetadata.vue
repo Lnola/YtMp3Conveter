407916f09d58e8c938d0dd43dc636fe73dc9b525
Lnola <nola.luka@gmail.com>
2023-08-05 01:11:22 +0200

    :sparkles: Add a thumbnail picker component

--- components/Video/VideoMetadata.vue
+++ components/Video/VideoMetadata.vue
@@ -1,13 +1,14 @@
 <template>
   <form @submit.prevent="submit">
+    <YouInput v-model="metadata.title" name="title" label="Title" />
     <YouInput v-model="metadata.artist" name="artist" label="Artist" />
     <YouInput v-model="metadata.category" name="category" label="Category" />
-    <YouInput v-model="metadata.title" name="title" label="Title" />
+    <YouThumbnail v-model="metadata.thumbnail" />
     <YouButton type="submit">Submit</YouButton>
   </form>
 </template>
 
-<script setup>
+<script setup lang="ts">
 const metadata = useMetadata();
 
 const submit = async () => {
---
+++ components/YouThumbnail.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="relative flex items-center w-48 h-48 bg-blue-700 cursor-pointer">
+    <img @click="select" :src="modelValue" alt="thumbnail" class="w-100%" />
+  </div>
+  <input class="hidden" @input="pick" ref="inputRef" type="file" />
+</template>
+
+<script setup lang="ts">
+const emits = defineEmits(['update:modelValue']);
+defineProps({
+  modelValue: { type: String, required: true },
+});
+
+const inputRef = ref<HTMLInputElement | null>(null);
+
+const select = () => {
+  if (!inputRef.value) return;
+  inputRef.value.click();
+};
+
+const pick = () => {
+  if (!inputRef.value) return;
+  const { files } = inputRef.value;
+  if (!files || !files[0]) return;
+  const reader = new FileReader();
+  reader.readAsDataURL(files[0]);
+  reader.onload = (event) => emits('update:modelValue', event.target?.result);
+};
+</script>
