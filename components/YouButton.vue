cf0f5ade6e7c18db7cea756ccef60a1186ef1707
Lnola <nola.luka@gmail.com>
2023-08-05 23:38:28 +0200

    :lipstick: Introduce a primary button variant

--- components/YouButton.vue
+++ components/YouButton.vue
@@ -1,7 +1,12 @@
 <template>
-  <button
-    class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none"
-  >
+  <button :class="ButtonVariants.PRIMARY">
     <slot></slot>
   </button>
 </template>
+
+<script setup lang="ts">
+const ButtonVariants = {
+  PRIMARY:
+    'px-12 py-3 bg-gradient bg-[length:200%] bg-right hover:bg-left transition-all duration-300 ease-in-out text-white tracking-wide font-bold rounded-full',
+};
+</script>
