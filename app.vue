5dff22513e9ebe9db4851526d2b5e656b195008c
Lnola <nola.luka@gmail.com>
2023-08-05 22:36:13 +0200

    :sparkles: Add a divider section

--- app.vue
+++ app.vue
@@ -1,7 +1,13 @@
 <template>
-  <VideoUrl @fetching="isLoading = $event" />
-  <YouLoader v-if="isLoading" />
-  <VideoMetadata v-if="shouldDisplayMetadata" />
+  <main class="h-screen" :class="{ 'overflow-scroll': shouldDisplayMetadata }">
+    <div class="h-screen">
+      <VideoUrl @fetching="isLoading = $event" />
+      <YouLoader v-if="isLoading" />
+      <VideoMetadata v-if="shouldDisplayMetadata" />
+    </div>
+
+    <img class="-mt-96" src="@/assets/baguettes.png" alt="divider" />
+  </main>
 </template>
 
 <script setup lang="ts">
@@ -9,3 +15,10 @@ const isLoading = ref(false);
 const metadata = useMetadata();
 const shouldDisplayMetadata = computed(() => metadata.value.artist.length > 0);
 </script>
+
+<style>
+body,
+#__nuxt {
+  overflow: hidden;
+}
+</style>
