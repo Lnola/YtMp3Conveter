3f19a834f68da84885ce1f6497f1afd61e0905e1
Lnola <nola.luka@gmail.com>
2023-08-05 23:38:10 +0200

    :bento: Create a custom gradient

---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+export default {
+  theme: {
+    extend: {
+      backgroundImage: {
+        gradient:
+          'linear-gradient(135deg, #f9e889, #fcae49, #ff6f03, #fcae49, #f9e889)',
+      },
+    },
+  },
+};
