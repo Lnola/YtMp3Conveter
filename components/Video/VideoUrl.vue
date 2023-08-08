f54ad2b1ed684e2908a990de21c1f8e61e6050c8
Lnola <nola.luka@gmail.com>
2023-08-08 21:53:39 +0200

    :sparkles: Fetch file if add metadata is unselected

--- components/Video/VideoUrl.vue
+++ components/Video/VideoUrl.vue
@@ -1,21 +1,47 @@
 <template>
-  <form @submit.prevent="submit">
-    <YouInput v-model="videoUrl" name="video-link" label="Youtube video link" />
-    <YouButton type="submit">Submit</YouButton>
+  <form
+    @submit.prevent="submit"
+    class="w-3/4 flex items-center justify-center flex-col"
+  >
+    <YouInput
+      v-model="videoUrl"
+      name="video-link"
+      label="Youtube video link"
+      class="mb-8 w-full"
+    />
+    <YouToggle v-model="shouldAddMetadata" name="toggle-metadata">
+      Add metadata
+    </YouToggle>
+    <YouButton type="submit">Convert</YouButton>
   </form>
 </template>
 
 <script setup lang="ts">
 const emits = defineEmits(['fetching']);
 
+const shouldAddMetadata = ref(false);
 const videoUrl = ref('');
 const metadata = useMetadata();
+const downloadFile = useDownloadFile();
 
-const submit = async () => {
-  emits('fetching', true);
+const fetchMetadata = async () => {
   const query = { videoUrl: videoUrl.value };
   const data = await $fetch('/api/audio/metadata', { query });
   metadata.value = { ...metadata.value, ...data };
+};
+
+const fetchDownloadFile = async () => {
+  const query = { videoUrl: videoUrl.value };
+  const data = await $fetch('/api/audio/download', { query });
+  downloadFile.value = data;
+};
+
+const submit = async () => {
+  emits('fetching', true);
+  const fetchMethod = shouldAddMetadata.value
+    ? fetchMetadata
+    : fetchDownloadFile;
+  await fetchMethod();
   videoUrl.value = '';
   emits('fetching', false);
 };
