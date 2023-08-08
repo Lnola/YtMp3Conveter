2b97520dd6b25b4e14156f1aef81b434983dd256
Lnola <nola.luka@gmail.com>
2023-08-08 22:39:09 +0200

    :bento: Add color orange to tailwind theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,9 @@
 export default {
   theme: {
     extend: {
+      colors: {
+        orange: '#fcaf4a',
+      },
       backgroundImage: {
         gradient:
           'linear-gradient(135deg, #f9e889, #fcae49, #ff6f03, #fcae49, #f9e889)',
