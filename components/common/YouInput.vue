c4f5b4d07f3842f30aea881b0cc7098b91a9d55c
Lnola <nola.luka@gmail.com>
2023-08-09 00:00:31 +0200

    :lipstick: Make inputs round

--- components/common/YouInput.vue
+++ components/common/YouInput.vue
@@ -1,17 +1,17 @@
 <template>
-  <div v-bind="$attrs" class="relative z-0 mt-2">
+  <div v-bind="$attrs" class="relative z-0 mt-4">
     <input
       @input="updateModelValue"
       :value="modelValue"
       :id="name"
       :aria-describedby="helpLabel"
       type="text"
-      class="block py-2.5 px-1.5 w-full text-sm text-gray-700 bg-transparent border-b-2 border-gray-300 appearance-none focus:outline-none focus:border-orange peer transition-colors duration-300"
+      class="block py-2.5 px-4 w-full text-sm text-gray-700 bg-transparent border-2 rounded-full border-gray-300 appearance-none focus:outline-none focus:border-orange peer transition-colors duration-300"
       placeholder=""
     />
     <label
       :for="name"
-      class="absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 left-0 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-orange peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:left-2 peer-focus:scale-75 peer-focus:-translate-y-6"
+      class="absolute text-sm text-gray-500 duration-300 transform -translate-y-8 scale-75 top-3 left-0 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-orange peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:left-4 peer-focus:scale-75 peer-focus:-translate-y-8"
     >
       {{ label }}
     </label>
