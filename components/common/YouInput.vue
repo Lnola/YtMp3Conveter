56356fb06f41e97109a0a2bd0e6d8ee9e641e5f6
Lnola <nola.luka@gmail.com>
2023-08-08 22:39:31 +0200

    :lipstick: Update input color styling

--- components/common/YouInput.vue
+++ components/common/YouInput.vue
@@ -6,12 +6,12 @@
       :id="name"
       :aria-describedby="helpLabel"
       type="text"
-      class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+      class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-b-2 border-gray-300 appearance-none focus:outline-none focus:border-orange peer"
       placeholder=""
     />
     <label
       :for="name"
-      class="absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+      class="absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-orange peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
     >
       {{ label }}
     </label>
@@ -37,3 +37,10 @@ const updateModelValue = (event: Event) => {
   emits('update:modelValue', value);
 };
 </script>
+
+<style scoped>
+input {
+  background-clip: text;
+  -webkit-background-clip: text;
+}
+</style>
