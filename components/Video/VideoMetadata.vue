ff1361907595c30d84afbd48feca613fe27324ed
Lnola <nola.luka@gmail.com>
2023-08-09 00:32:59 +0200

    :lipstick: Style metadata inputs

--- components/Video/VideoMetadata.vue
+++ components/Video/VideoMetadata.vue
@@ -1,14 +1,50 @@
 <template>
-  <form @submit.prevent="submit">
-    <YouInput v-model="metadata.artist" name="artist" label="Artist" />
-    <YouInput v-model="metadata.album" name="album" label="Album" />
-    <YouInput v-model="metadata.category" name="category" label="Category" />
-    <YouInput v-model="metadata.filename" name="filename" label="Filename" />
+  <form @submit.prevent="submit" class="w-7/12">
     <YouInput v-model="metadata.title" name="title" label="Title" />
-    <YouInput v-model="metadata.year" name="year" label="Year" />
-    <YouInput v-model="metadata.genre" name="genre" label="Genre" />
+    <section class="grid grid-cols-2 gap-4">
+      <div>
+        <YouInput
+          v-model="metadata.filename"
+          name="filename"
+          label="Filename"
+          class="mt-8"
+        />
+        <YouInput
+          v-model="metadata.artist"
+          name="artist"
+          label="Artist"
+          class="mt-8"
+        />
+        <YouInput
+          v-model="metadata.album"
+          name="album"
+          label="Album"
+          class="mt-8"
+        />
+      </div>
+      <div>
+        <YouInput
+          v-model="metadata.category"
+          name="category"
+          label="Category"
+          class="mt-8"
+        />
+        <YouInput
+          v-model="metadata.year"
+          name="year"
+          label="Year"
+          class="mt-8"
+        />
+        <YouInput
+          v-model="metadata.genre"
+          name="genre"
+          label="Genre"
+          class="mt-8"
+        />
+      </div>
+    </section>
     <!-- <YouThumbnail v-model="metadata.thumbnail" /> -->
-    <YouButton type="submit">Submit</YouButton>
+    <YouButton type="submit" class="w-full mt-8">Submit</YouButton>
   </form>
 </template>
 
