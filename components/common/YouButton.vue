5b6d47875a128efabb1c903b1883da553c4bc07e
Lnola <nola.luka@gmail.com>
2023-08-09 00:14:51 +0200

    :bug: Fix button outline

--- components/common/YouButton.vue
+++ components/common/YouButton.vue
@@ -7,6 +7,6 @@
 <script setup lang="ts">
 const ButtonVariants = {
   PRIMARY:
-    'px-12 py-3 bg-gradient bg-[length:200%] bg-right hover:bg-left transition-all duration-300 ease-in-out text-white tracking-wide font-bold rounded-full',
+    'px-12 py-3 bg-gradient bg-[length:200%] bg-right hover:bg-left transition-all duration-300 ease-in-out text-white tracking-wide font-bold rounded-full outline-orange',
 };
 </script>
