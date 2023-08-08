d48fb47199ad8798448e02b18d3fd0ec80467ad7
Lnola <nola.luka@gmail.com>
2023-08-08 23:48:52 +0200

    :bug: Use watch on refs instead of shouldDisplay

--- app.vue
+++ app.vue
@@ -8,15 +8,19 @@
     <div class="h-screen flex items-center justify-center flex-col">
       <VideoUrl @fetching="isLoading = $event" />
     </div>
-    <div class="h-screen flex items-center justify-center flex-col">
-      <VideoMetadata
-        ref="metadataRef"
-        v-if="shouldDisplayMetadata"
-        @fetching="isLoading = $event"
-      />
+    <div
+      v-if="shouldDisplayMetadata"
+      ref="metadataRef"
+      class="h-screen flex items-center justify-center flex-col"
+    >
+      <VideoMetadata @fetching="isLoading = $event" />
     </div>
-    <div class="h-screen flex items-center justify-center flex-col">
-      <VideoDownload v-if="shouldDisplayDownload" />
+    <div
+      v-if="shouldDisplayDownload"
+      ref="downloadRef"
+      class="h-screen flex items-center justify-center flex-col"
+    >
+      <VideoDownload />
     </div>
   </main>
 </template>
@@ -33,25 +37,19 @@ const shouldDisplayDownload = computed(() => downloadFile.value);
 
 const scrollRef = ref<Nullable<HTMLElement>>(null);
 const metadataRef = ref<Nullable<HTMLElement>>(null);
+const downloadRef = ref<Nullable<HTMLElement>>(null);
 const { x: _, y: scrollHeight } = useScroll(scrollRef, { behavior: 'smooth' });
 
 const scrollIntoMetadata = () => {
-  if (!metadataRef.value) return;
   scrollHeight.value = window.innerHeight;
 };
 
 const scrollIntoDownload = () => {
-  if (!metadataRef.value) return;
   scrollHeight.value = window.innerHeight * 2;
 };
 
-watchEffect(() => {
-  if (shouldDisplayMetadata.value) scrollIntoMetadata();
-});
-
-watchEffect(() => {
-  if (shouldDisplayDownload.value) scrollIntoDownload();
-});
+watch(metadataRef, scrollIntoMetadata);
+watch(downloadRef, scrollIntoDownload);
 </script>
 
 <style>
