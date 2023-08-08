7e8fa1dde29f9d2933d48f93710caee925ed9261
Lnola <nola.luka@gmail.com>
2023-08-09 00:29:18 +0200

    :lipstick: Add a focused state to toggle

--- components/common/YouToggle.vue
+++ components/common/YouToggle.vue
@@ -8,9 +8,9 @@
       class="sr-only peer"
     />
     <div
-      class="w-11 h-6 bg-gray-200 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-orange"
+      class="w-11 h-6 bg-gray-200 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-orange"
     ></div>
-    <span class="ml-3 text-sm text-gray-700">
+    <span class="ml-3 text-sm text-gray-700 peer-focus:text-orange">
       <slot></slot>
     </span>
   </label>
