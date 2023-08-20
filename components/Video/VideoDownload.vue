dc65ce62e9cd03e122bc77791d8badb381d71e5c
Lnola <nola.luka@gmail.com>
2023-08-20 20:05:55 +0200

    :lipstick: Finish the video download

--- components/Video/VideoDownload.vue
+++ components/Video/VideoDownload.vue
@@ -1,13 +1,30 @@
 <template>
   <template v-if="downloadFile">
-    <a :href="downloadFile.outputUrl" :download="downloadFile.filename">
-      Download
-    </a>
-    <button @click="emits('retry')">Try again</button>
+    <iframe
+      class="mb-6 rounded-lg"
+      width="512"
+      height="288"
+      :src="embededVideo"
+    ></iframe>
+    <div class="flex gap-6">
+      <YouButton
+        :href="downloadFile.outputUrl"
+        :download="downloadFile.filename"
+        >Download
+      </YouButton>
+      <YouButton @click="emits('retry')" variant="SECONDARY">
+        Try again
+      </YouButton>
+    </div>
   </template>
 </template>
 
 <script setup lang="ts">
 const emits = defineEmits(['retry']);
+const metadata = useMetadata();
 const downloadFile = useDownloadFile();
+
+const embededVideo = computed(() =>
+  metadata.value.videoUrl.replace('watch?v=', 'embed/')
+);
 </script>
