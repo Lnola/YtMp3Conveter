843b4ea84ac27dfea6d7ee9b658d0e3658791b61
Lnola <nola.luka@gmail.com>
2023-08-20 20:25:52 +0200

    :truck: Expose button variants

--- components/common/YouButton.vue
+++ components/common/YouButton.vue
@@ -1,17 +1,20 @@
 <template>
-  <button :class="buttonVariant">
+  <button v-bind="$attrs" :class="buttonVariant">
     <slot></slot>
   </button>
 </template>
 
-<script setup lang="ts">
-const Variants = {
+<script lang="ts">
+export const Variants = {
   PRIMARY:
     'px-12 py-3 bg-gradient bg-[length:200%] bg-right hover:bg-left transition-all duration-300 ease-in-out text-white tracking-wide font-bold rounded-full outline-orange',
   SECONDARY:
     'px-12 py-3 border-4 border-orange bg-transparent transition-all duration-300 ease-in-out text-orange tracking-wide font-bold rounded-full outline-orange',
 };
-export type Variant = 'PRIMARY' | 'SECONDARY';
+</script>
+
+<script setup lang="ts">
+type Variant = 'PRIMARY' | 'SECONDARY';
 
 const props = defineProps({
   variant: { type: String as PropType<Variant>, default: 'PRIMARY' },
